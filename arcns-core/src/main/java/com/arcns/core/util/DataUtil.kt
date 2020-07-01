460c2eb56801daa049f0962e574a1ca921e823ba
Arcns <457194471@qq.com>
2020-07-01 14:08:43 +0800

    update

--- arcns-core/src/main/java/com/arcns/core/util/DataUtil.kt
+++ arcns-core/src/main/java/com/arcns/core/util/DataUtil.kt
@@ -72,3 +72,68 @@ fun str2Hex(letter: String): String? {
     sb.deleteCharAt(sb.length - 2)
     return sb.toString()
 }
+
+/**
+ * 计算CRC-16/MODBU值
+ * 对应C语言端代码如下：
+ *
+ * u16 CRC16_Check(u8 *Pushdata,u16 length)
+ * {
+ * u16 Reg_CRC=0xffff;
+ * u8 Temp_reg=0x00;
+ * u16 i,j;
+ *
+ * for( i = 0; i<length></length>; i ++)
+ * {
+ * Reg_CRC^= *Pushdata++;
+ *
+ * for (j = 0; j<8; j++)
+ * {
+ * if (Reg_CRC & 0x0001)
+ * Reg_CRC=Reg_CRC>>1^0xA001;
+ * else
+ * Reg_CRC >>=1;
+ *
+ * }
+ * }
+ * return (Reg_CRC);
+ * }
+ * @param data
+ * @return
+ */
+fun calculatedCRC16(data: ByteArray): Int {
+    var Reg_CRC = 0xffff
+    var temp: Int
+    var j: Int
+    var i = 0
+    while (i < data.size) {
+        temp = data[i].toInt()
+        if (temp < 0) temp += 256
+        temp = temp and 0xff
+        Reg_CRC = Reg_CRC xor temp
+        j = 0
+        while (j < 8) {
+            Reg_CRC =
+                if (Reg_CRC and 0x0001 == 0x0001) Reg_CRC shr 1 xor 0xA001 else Reg_CRC shr 1
+            j++
+        }
+        i++
+    }
+    return Reg_CRC and 0xffff
+}
+
+/**
+ * 计算CRC-16/MODBU值
+ * @param dataStr
+ * @return
+ */
+fun calculatedCRC16ToHex(dataStr: String, charset: String = "GB2312"): String? {
+    return try {
+        Integer.toHexString(
+            calculatedCRC16(dataStr.toByteArray(charset(charset)))
+        )
+    } catch (e: Exception) {
+        e.printStackTrace()
+        null
+    }
+}
