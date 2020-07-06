27aa95bd1f7486a2b11895c5d4b5ecfd9c61f76f
Arcns <457194471@qq.com>
2020-07-06 14:02:32 +0800

    update

--- arcns-core/src/main/java/com/arcns/core/util/AutoClearedValue.kt
+++ arcns-core/src/main/java/com/arcns/core/util/AutoClearedValue.kt
@@ -11,7 +11,54 @@ import kotlin.reflect.KProperty
  * 使对象能够在Fragment销毁（onDestroyView）时自动设置为NULL
  * 可解决DataBinding等对象造成Fragment内存泄漏等问题
  */
-class AutoClearedValue<T : Any?>(
+class AutoClearedValue<T : Any>(
+    val fragment: Fragment,
+    val onCreateCallback: (() -> T?)? = null,
+    val onClearCallback: ((value: T?) -> Unit)? = null
+) :
+    ReadWriteProperty<Fragment, T> {
+    private var _value: T? = null
+
+    init {
+        fragment.lifecycle.addObserver(object : LifecycleObserver {
+            @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
+            fun onCreate() {
+                fragment.viewLifecycleOwnerLiveData.observe(fragment, Observer {
+                    it.lifecycle.addObserver(object : LifecycleObserver {
+                        @OnLifecycleEvent(Lifecycle.Event.ON_CREATE)
+                        fun onCreate() {
+                            onCreateCallback?.invoke()?.run {
+                                _value = this
+                            }
+                        }
+
+                        @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+                        fun onDestroy() {
+                            onClearCallback?.invoke(_value)
+                            _value = null
+                        }
+                    })
+                })
+            }
+        })
+    }
+
+    override fun getValue(thisRef: Fragment, property: KProperty<*>): T {
+        return _value ?: throw IllegalStateException(
+            "should never call auto-cleared-value get when it might not be available"
+        )
+    }
+
+    override fun setValue(thisRef: Fragment, property: KProperty<*>, value: T) {
+        _value = value
+    }
+}
+
+/**
+ * 使对象能够在Fragment销毁（onDestroyView）时自动设置为NULL
+ * 可解决DataBinding等对象造成Fragment内存泄漏等问题
+ */
+class AutoClearedValueOrNull<T : Any?>(
     val fragment: Fragment,
     val onCreateCallback: (() -> T?)? = null,
     val onClearCallback: ((value: T?) -> Unit)? = null
@@ -43,9 +90,7 @@ class AutoClearedValue<T : Any?>(
         })
     }
 
-    override fun getValue(thisRef: Fragment, property: KProperty<*>): T? {
-        return _value
-    }
+    override fun getValue(thisRef: Fragment, property: KProperty<*>): T? = _value
 
     override fun setValue(thisRef: Fragment, property: KProperty<*>, value: T?) {
         _value = value
@@ -55,12 +100,12 @@ class AutoClearedValue<T : Any?>(
 /**
  * 使对象能够在Activty销毁（onDestroy）时自动设置为NULL
  */
-class ActivityAutoClearedValue<T : Any?>(
+class ActivityAutoClearedValue<T : Any>(
     val activity: ComponentActivity,
     val onCreateCallback: (() -> T?)? = null,
     val onClearCallback: ((value: T?) -> Unit)? = null
 ) :
-    ReadWriteProperty<ComponentActivity, T?> {
+    ReadWriteProperty<ComponentActivity, T> {
     private var _value: T? = null
 
     init {
@@ -80,11 +125,13 @@ class ActivityAutoClearedValue<T : Any?>(
         })
     }
 
-    override fun getValue(thisRef: ComponentActivity, property: KProperty<*>): T? {
-        return _value
+    override fun getValue(thisRef: ComponentActivity, property: KProperty<*>): T {
+        return _value ?: throw IllegalStateException(
+            "should never call auto-cleared-value get when it might not be available"
+        )
     }
 
-    override fun setValue(thisRef: ComponentActivity, property: KProperty<*>, value: T?) {
+    override fun setValue(thisRef: ComponentActivity, property: KProperty<*>, value: T) {
         _value = value
     }
 }
@@ -93,7 +140,17 @@ class ActivityAutoClearedValue<T : Any?>(
  * 使用方法：var obj by autoCleared<Obj>()
  * 例如：var binding by autoCleared<FragmentBinding>()
  */
-fun <T : Any?> Fragment.autoCleared(
+fun <T : Any?> Fragment.autoClearedOrNull(
+    onCreateCallback: (() -> T?)? = null,
+    onClearCallback: ((value: T?) -> Unit)? = null
+) =
+    AutoClearedValueOrNull(this, onCreateCallback, onClearCallback)
+
+/**
+ * 使用方法：var obj by autoCleared<Obj>()
+ * 例如：var binding by autoCleared<FragmentBinding>()
+ */
+fun <T : Any> Fragment.autoCleared(
     onCreateCallback: (() -> T?)? = null,
     onClearCallback: ((value: T?) -> Unit)? = null
 ) =
@@ -104,7 +161,7 @@ fun <T : Any?> Fragment.autoCleared(
  * 使用方法：var obj by autoCleared<Obj>()
  * 例如：var binding by autoCleared<FragmentBinding>()
  */
-fun <T : Any?> ComponentActivity.autoCleared(
+fun <T : Any> ComponentActivity.autoCleared(
     onCreateCallback: (() -> T?)? = null,
     onClearCallback: ((value: T?) -> Unit)? = null
 ) =
