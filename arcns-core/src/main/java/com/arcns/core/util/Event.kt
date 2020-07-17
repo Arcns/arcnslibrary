a0c9ce10fda046ecebdbd8e28abf2b09afaef9ac
Arcns <457194471@qq.com>
2020-07-17 13:58:50 +0800

    update

--- arcns-core/src/main/java/com/arcns/core/util/Event.kt
+++ arcns-core/src/main/java/com/arcns/core/util/Event.kt
@@ -4,24 +4,35 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
 import java.lang.Exception
 
-open class Event<out T>(private val content: T) {
+open class Event<out T>(content: T) {
 
     var hasBeenHandled = false
         private set // Allow external read but not write
+    private var eventContent: Any? = null
 
-    fun getContentIfNotHandled(getContentCallback: (T) -> Unit) {
+    init {
+        eventContent = content
+    }
+
+    fun getContentIfNotHandled(isDisposable: Boolean = false, getContentCallback: (T) -> Unit) {
         if (!hasBeenHandled) {
             hasBeenHandled = true
-            getContentCallback(content)
+            (eventContent as? T)?.run {
+                getContentCallback(this)
+            }
+            if (isDisposable) eventContent = null
         }
     }
 
-    fun peekContent(): T = content
+    fun peekContent(): T? = eventContent as? T
 }
 
-class EventObserver<T>(private val onEventUnhandledContent: (T) -> Unit) : Observer<Event<T>> {
+class EventObserver<T>(
+    private val isDisposable: Boolean = false, //是否为一次性数据，如果为一次性数据，则数据会在取出后自动置null
+    private val onEventUnhandledContent: (T) -> Unit
+) : Observer<Event<T>> {
     override fun onChanged(event: Event<T>) {
-        event.getContentIfNotHandled {
+        event.getContentIfNotHandled(isDisposable) {
             onEventUnhandledContent(it)
         }
     }
